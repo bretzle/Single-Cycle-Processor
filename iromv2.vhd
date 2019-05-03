e8098b67b4b9e71080f353e3d239f4c40149ba50
John <johnfish218@gmail.com>
2019-05-03 14:42:36 -0500

    Added new iroms

--- iromv2.vhd
+++ iromv2.vhd
@@ -1,7 +1,7 @@
 --*********************************************************
 --* FILENAME: irom.vhd
 --* AUTHOR:   meier@msoe.edu <Dr. M>
---* DATE:     Spring Quarter 2019
+--* DATE:     Spring Quarter 2016
 --* PROVIDES: 
 --* - a test program for the first single cycle processor
 --* - calculates the sum of the first n integers
@@ -44,23 +44,21 @@ architecture dataflow of irom is
 begin
  
 	with A select RD <= 
-		X"E3A0_800A" when X"00000000",  -- MOV 
-      X"E3A0_9000" when X"00000004",  -- MOV
-      X"E358_0000" when X"00000008",  -- CMP
-      X"0A00_000B" when X"0000000C",  -- BEQ
-      X"E089_9008" when X"00000010",  -- ADD
-      X"E248_8001" when X"00000014",  -- SUB
-		X"E358_0000" when X"00000018",  -- CMP
-		X"1AFF_FFF9" when X"0000001C",  -- BNE
-		X"E3A0_A000" when X"00000020",  -- MOV
-		X"E24A_A020" when X"00000024",  -- SUB
-      X"E009_A00A" when X"00000028",  -- AND
-      X"E35A_0000" when X"0000002C",  -- CMP
-		X"0A00_0002" when X"00000030",  -- BEQ
-		X"E3A0_B001" when X"00000034",  -- MOV
-		X"E3A0_C004" when X"00000038",  -- MOV
-      X"E58C_B000" when X"0000003C",  -- STR
-		X"E59C_6000" when X"00000040",  -- LDR
-		X"EAFF_FFFD" when others;		  -- END
+		X"E3A0_9000" when X"00000000",
+      X"E3B0_800A" when X"00000004",  
+      X"0A00_0009" when X"00000008",
+      X"E089_9008" when X"0000000C",
+      X"E258_8001" when X"00000010", 
+		X"1AFF_FFFC" when X"00000014",
+		X"E3A0_A000" when X"00000018",
+		X"E24A_A020" when X"0000001C",
+      X"E019_A00A" when X"00000020",
+		X"0A00_0002" when X"00000024",
+		X"E3A0_B001" when X"00000028",
+		X"E3A0_C004" when X"0000002C",
+      X"E58C_B000" when X"00000030",
+		X"E59C_6000" when X"00000034",
+		X"EAFF_FFFD" when others;
 		  
 end architecture dataflow;
+
